6ddfd2848a7a1f1e11dfcfdf68cee9ece2887e1f
altaafali092 <altaafali092@gmail.com>
2026-02-20 07:56:24 +0545

    feat: create `admission_queries` table with fields for student, parent, and previous school information.

--- app/Models/AdmissionQuery.php
+++ app/Models/AdmissionQuery.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use App\Traits\FileTrait;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AdmissionQuery extends Model
 {
-    //
+    use HasFactory, SoftDeletes, FileTrait;
+
+    protected $fillable = [
+        'full_name',
+        'date_of_birth',
+        'gender',
+        'grade_applying_for',
+        'nationality',
+        'father_name',
+        'mother_name',
+        'parent_phone',
+        'parent_email',
+        'parent_address',
+        'previous_school_name',
+        'previous_last_grade',
+        'medical_info',
+        'require_attend',
+        'remarks',
+        'documents',
+    ];
+
+    protected $casts = [
+        'date_of_birth' => 'date',
+    ];
+    public function documents(): Attribute
+    {
+        return $this->castingFile(defaultPath: 'admission-query-documents');
+    }
 }
--- database/migrations/2026_02_20_015411_create_admission_queries_table.php
+++ database/migrations/2026_02_20_015411_create_admission_queries_table.php
@@ -13,6 +13,23 @@ return new class extends Migration
     {
         Schema::create('admission_queries', function (Blueprint $table) {
             $table->id();
+            $table->string('full_name');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->string('grade_applying_for');
+            $table->string('nationality');
+            $table->string('father_name');
+            $table->string('mother_name');
+            $table->string('parent_phone');
+            $table->string('parent_email');
+            $table->string('parent_address');
+            $table->string('previous_school_name');
+            $table->string('previous_last_grade');
+            $table->string('medical_info')->nullable();
+            $table->string('require_attend')->nullable();
+            $table->text('remarks')->nullable();
+            $table->string('documents')->nullable();
+
             $table->timestamps();
         });
     }
