196349a8a7f765fca73957eae53737bfc4ac6d57
altaafali092 <altaafali092@gmail.com>
2026-03-09 07:28:17 +0545

    feat: Implement Academics page to showcase academic levels and programs with dynamic navigation and detailed content.

--- resources/js/pages/frontend/Academics.tsx
+++ resources/js/pages/frontend/Academics.tsx
@@ -5,7 +5,7 @@ import { ArrowRight, GraduationCap, } from 'lucide-react';
 import * as LucideIcons from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { AcademicLevel } from '@/types/Frontend/Academics';
-import { academicsShow, home } from '@/routes';
+import { academicsShow, admissions, contact, home } from '@/routes';
 import parse from 'html-react-parser';
 
 interface AcademicItem {
@@ -163,11 +163,11 @@ const Academics = ({ levels, academicLevel }: AcademicProps) => {
                             <p className="text-slate-500 dark:text-slate-400 font-bold text-xs lg:text-sm uppercase tracking-widest max-w-2xl mx-auto">Take the first step towards excellence by enrolling in our 2026-27 session. Limited seats available for specialized streams.</p>
                         </div>
                         <div className="flex flex-col sm:flex-row items-center justify-center gap-6">
-                            <Link href="#" className="w-full sm:w-auto h-15 px-12 bg-blue-600 dark:bg-blue-600 text-white rounded-4xl font-black text-sm uppercase tracking-widest hover:bg-white hover:text-blue-600 dark:hover:text-white transition-all flex items-center justify-center gap-4 italic shadow-2xl shadow-blue-600/20 dark:shadow-blue-900/40 group">
+                            <Link href={admissions()} className="w-full sm:w-auto h-15 px-12 bg-blue-600 dark:bg-blue-600 text-white rounded-4xl font-black text-sm uppercase tracking-widest hover:bg-white hover:text-blue-600 dark:hover:text-white transition-all flex items-center justify-center gap-4 italic shadow-2xl shadow-blue-600/20 dark:shadow-blue-900/40 group">
                                 Apply for Admission
                                 <ArrowRight className="w-5 h-5 group-hover:translate-x-2 transition-transform" />
                             </Link>
-                            <Link href="#" className="w-full sm:w-auto h-15 px-12 bg-white/5 dark:bg-slate-900/50 border border-white/10 dark:border-slate-800 text-white rounded-4xl font-black text-sm uppercase tracking-widest hover:bg-white hover:text-slate-950 transition-all flex items-center justify-center gap-4 italic shadow-2xl">
+                            <Link href={contact()} className="w-full sm:w-auto h-15 px-12 bg-white/5 dark:bg-slate-900/50 border border-white/10 dark:border-slate-800 text-white rounded-4xl font-black text-sm uppercase tracking-widest hover:bg-white hover:text-slate-950 transition-all flex items-center justify-center gap-4 italic shadow-2xl">
                                 Request Prospectus
                             </Link>
                         </div>
